559640195a2a7b2effa288ba3a5327f0f88ee8e7
dinesh <dineshubli@gmail.com>
2024-07-16 15:29:22 +0530

    logo updated

--- src/Components/Aboutme.jsx
+++ src/Components/Aboutme.jsx
@@ -9,14 +9,14 @@ const Aboutme = () => {
         <div className='flex flex-col lg:flex-row items-center'>
           <img src={logo} alt="Dinesh's Logo" className='w-40 lg:w-48 h-auto mr-0 lg:mr-4 mb-4 lg:mb-0 rounded-full border-2 border-[#90A0D9]' />
           <p className='text-white pb-4 lg:pl-4 lg:text-lg '>
-            Hello! I'm <span className='text-[#90A0D9]'>Dinesh</span>, a dedicated and meticulous front-end developer with a passion for creating visually stunning and interactive user experiences. My journey into the world of web development ignited from a curiosity about designing aesthetically pleasing websites, evolving into a mastery of diverse technologies.
+            Hello! I'm <span className='text-[#90A0D9]'>Dinesh</span>,  a dedicated and meticulous MERN (MongoDB, Express.js, React.js, Node.js) developer with a passion for creating visually stunning and interactive web applications. My journey into web development began with a curiosity for building dynamic and responsive websites, evolving into a mastery of the full-stack MERN technologies.
           </p>
-        </div> 
+        </div>
         <p className='text-white pb-4 lg:pl-4 lg:text-lg '>
-          As a recent graduate, I bring a fresh perspective, an insatiable appetite for learning, and an unwavering commitment to staying abreast of the latest trends in web development. My aspiration is to contribute to captivating projects, collaborate with talented teams, and continually elevate my skills in this ever-evolving field.
+          As a recent graduate, I bring a fresh perspective, an insatiable appetite for learning, and a strong commitment to staying up-to-date with the latest trends in web development. My aspiration is to contribute to innovative projects, collaborate with talented teams, and continually enhance my skills in this ever-evolving field.
         </p>
         <p className='text-white pb-4 lg:pl-4 lg:text-lg'>
-          I am currently on the lookout for opportunities to apply and broaden my skills, make meaningful contributions to projects, and evolve as a front-end developer. Let's embark on the journey of building something extraordinary together!
+          I am currently seeking opportunities to apply and expand my expertise, make meaningful contributions to projects, and grow as a MERN developer. Let's embark on the journey of building something extraordinary together!
         </p>
       </div>
     </div>
